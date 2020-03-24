83238c250e0920b05de3cdf3e980466a2a018432
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-03-24 16:01:55 -0400

    Hice la entidad usuarios

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataVentas.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            NombreUsuario = string.Empty;
+            Email = string.Empty;
+            Clave = string.Empty;
+        }
+    }
+}
